137f3e212d108c9f13ba4bcf34f8f114aed1ab21
renuka1402 <sharma1402renu@gmail.com>
2024-05-15 06:45:56 +0530

    Add files via upload

---
+++ mega.js
@@ -0,0 +1,348 @@
+function hello(){
+
+    let link1= `   <div id="showmega">
+    <ul>
+        <li><a href="#">shop</a></li>
+        <li><a href="#"><h1>Shop the Latest</h1></a></li>
+        <li><a href="#"><h1>Mac</h1></a></li>
+        <li><a href="#"><h1>ipad</h1></a></li>
+        <li><a href="#"><h1>iphone</h1></a></li>
+        <li><a href="#"><h1>Apple Watch</h1></a></li>
+        <li><a href="#"><h1>Accessories</h1></a></li>
+
+       
+    </ul>
+    <ul >
+        <li><a href="#">Quick Links</a></li>
+        <li><a href="#"><h4>Find a store</h4></a></li>
+        <li><a href="#"><h4>Order Status</h4></a></li>
+        <li><a href="#"><h4>Way to buy</h4></a></li>
+        <li><a href="#"><h4>University Student Offer</h4></a></li>
+       
+    </ul>
+    <ul>
+        <li><a href="#">Shop special store</a></li>
+        <li><a href="#"><h4>Education</h4></a></li>
+        <li><a href="#"><h4>Business</h4></a></li>
+    
+    </ul>
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link1;
+ }
+ 
+ function hiddennav(){
+     document.getElementById('finaloutput').innerHTML="";
+ }
+ function tophide()
+ {
+     let clienth=window.event.clientY;
+     if(clienth<20)
+     {
+         document.getElementById('finaloutput').innerHTML="";  
+     }
+ }
+ function mac(){
+    let link2=`<div id="ipad">
+    <ul>
+        <li><a href="#">Explore Mac</a></li>
+        <li><a href="#"><h1>Explore All Mac</h1></a></li>
+        <li><a href="#"><h1>MacBook Air</h1></a></li>  
+        <li><a href="#"><h1>MacBook pro</h1></a></li>
+        
+        <li><a href="#"><h1>iMac</h1></a></li>
+        <li><a href="#"><h1>Mac mini</h1></a></li>
+        <li><a href="#"><h1>Mac studio</h1></a></li>
+        <li><a href="#"><h1>Mac pro</h1></a></li>
+      
+        <li><a href="#"><h1>iMac</h1></a></li>
+        <li><a href="#"><h1>Display</h1></a></li>
+        <li><a href="#">compare Mac</a></li>
+        <li><a href="#">Mac Does That</a></li>
+       
+    </ul>
+    <ul>
+        <li><a href="#">Mac</a></li>
+        <li><a href="#">Support</a></li>
+        <li><a href="#">ipad</a></li>
+        <li><a href="#">Phone</a></li>
+        <li><a href="#">Vision</a></li>
+        <li><a href="#">watch</a></li>
+        <li><a href="#">telivision</a></li>
+        <li><a href="#">entertenment</a></li>
+        <li><a href="#">Accessories</a></li>
+        <li><a href="#">Support</a></li>
+       
+    </ul>
+    <ul>
+        <li><a href="#">Mac</a></li>
+        <li><a href="#">Support</a></li>
+        <li><a href="#">ipad</a></li>
+        <li><a href="#">Phone</a></li>
+        <li><a href="#">Vision</a></li>
+      
+       
+    </ul>
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link2;
+ }
+ function ipad(){
+    let link3=`        <div id="mac">
+    <ul>
+    <li><a href="#">Explore ipad</a></li>
+    <li><a href="#"><h1>Explore All ipad</h1></a></li>
+    <li><a href="#"><h1>ipad pro</h1></a></li>
+    <li><a href="#"><h1>ipad Air</h1></a></li>
+    <li><a href="#"><h1>iPad mini</h1></a></li>
+    <li><a href="#"><h1>Apple Pencil</h1></a></li>
+    <li><a href="#"><h1>Keyboard</h1></a></li>
+    <li><a href="#">compare iPad</a></li>
+    <li><a href="#">Why iPad</a></li>
+    </ul>
+    <ul>
+
+    <li><a href="#">shop iPad</a></li>
+    <li><a href="#"><h4>Shop iPad</h4></a></li>
+    <li><a href="#"><h4>iPad Accessories</h4></a></li>
+    <li><a href="#"><h4>Ways to Buy</h4></a></li>
+    <li><a href="#"><h4>Accessibility</h4></a></li>
+       
+    </ul>
+    <ul>
+    <li><a href="#">More from iPad</a></li>
+    <li><a href="#"><h4>iPad Support</h4></a></li>
+    <li><a href="#"><h4>iPadOS 17</h4></a></li>
+    <li><a href="#"><h4>iCloud+</h4></a></li>
+    <li><a href="#"><h4>Applecare +</h4></a></li>
+    <li><a href="#"><h4> Logic iPad</h4></a></li>
+    <li><a href="#"><h4>Education</h4></a></li>
+  
+       
+    </ul>
+    </div> ; `;
+    document.getElementById('finaloutput').innerHTML= link3;
+ }
+
+ function phone(){
+    let link4=`   <div id="phone">
+    <ul>
+
+    <li><a href="#"><h1>Shop the Latest</h1></a></li>
+    <li><a href="#"><h1>Mac</h1></a></li>
+    <li><a href="#"><h1>ipad</h1></a></li>
+    <li><a href="#"><h1>iphone</h1></a></li>
+    <li><a href="#"><h1>Apple Watch</h1></a></li>
+    <li><a href="#"><h1>Accessories</h1></a></li>
+    </ul>
+    
+    <ul>
+    <li><a href="#">More from iPad</a></li>
+    <li><a href="#"><h4>iPad Support</h4></a></li>
+    <li><a href="#"><h4>iPadOS 17</h4></a></li>
+    <li><a href="#"><h4>iCloud+</h4></a></li>
+    <li><a href="#"><h4>Applecare +</h4></a></li>
+    <li><a href="#"><h4> Logic iPad</h4></a></li>
+    <li><a href="#"><h4>Education</h4></a></li>
+    </ul>
+    <ul>
+    <li><a href="#">shop iPad</a></li>
+    <li><a href="#"><h4>Shop iPad</h4></a></li>
+    <li><a href="#"><h4>iPad Accessories</h4></a></li>
+    <li><a href="#"><h4>Ways to Buy</h4></a></li>
+    <li><a href="#"><h4>Accessibility</h4></a></li>
+       
+    </ul>
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link4;
+ }
+ function vision(){
+    let link5=`<div id="vn">
+    <ul>
+    <li><a href="#">Explore Support</a></li>
+    <li><a href="#"><h1>iphone</h1></a></li>
+    <li><a href="#"><h1>Mac</h1></a></li>
+    <li><a href="#"><h1>ipad</h1></a></li>
+    <li><a href="#"><h1>Watch</h1></a></li>
+    <li><a href="#"><h1>AirPods</h1></a></li>
+    <li><a href="#"><h1>Music</h1></a></li>
+    <li><a href="#"><h1>TV</h1></a></li>
+    <li><a href="#">Explore Support</a></li>
+       
+    </ul>
+    <ul>
+    <li><a href="#">Quick Links</a></li>
+    <li><a href="#"><h4>Find a store</h4></a></li>
+    <li><a href="#"><h4>Order Status</h4></a></li>
+    <li><a href="#"><h4>Way to buy</h4></a></li>
+    <li><a href="#"><h4>University Student Offer</h4></a></li>
+       
+    </ul>
+
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link5;
+ }
+ function watch(){
+    let link6=`   <div id="wth">
+    <ul>
+    <li><a href="#"><h1>ipad Air</h1></a></li>
+    <li><a href="#"><h1>iPad mini</h1></a></li>
+    <li><a href="#"><h1>Apple Pencil</h1></a></li>
+    <li><a href="#"><h1>Keyboard</h1></a></li>
+    <li><a href="#">compare iPad</a></li>
+    <li><a href="#">Why iPad</a></li>
+    </ul>
+    <ul>
+    <li><a href="#">Helpful Topics</a></li>
+    <li><a href="#"><h4>Get AppleCare+</h4></a></li>
+    <li><a href="#"><h4>Apple ID & Password</h4></a></li>
+    <li><a href="#"><h4>Find My</h4></a></li>
+    <li><a href="#"><h4>Accessibility</h4></a></li>
+    <li><a href="#"><h4>Community</h4></a></li>
+    <li><a href="#"><h4> Check Coverage</h4></a></li>
+    <li><a href="#"><h4>Repaid</h4></a></li>
+    <li><a href="#"><h4>Contact us</a></li>
+       
+    </ul>
+    <ul>
+   
+    <li><a href="#">Get Help</a></li>
+    <li><a href="#"><h4>Community</h4></a></li>
+    <li><a href="#"><h4> Check Coverage</h4></a></li>
+    <li><a href="#"><h4>Repaid</h4></a></li>
+    <li><a href="#"><h4>Contact us</a></li>
+
+    </ul>
+    
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link6;
+ }
+ 
+ function teli(){
+    let link7=`   <div id="teli">
+    <ul>
+    <li><a href="#">shop</a></li>
+    <li><a href="#"><h1>Shop the Latest</h1></a></li>
+    <li><a href="#"><h1>Mac</h1></a></li>
+    <li><a href="#"><h1>ipad</h1></a></li>
+    <li><a href="#"><h1>iphone</h1></a></li>
+    <li><a href="#"><h1>Apple Watch</h1></a></li>
+    <li><a href="#"><h1>Accessories</h1></a></li>
+       
+    </ul>
+    <ul>
+    <li><a href="#">Quick Links</a></li>
+    <li><a href="#"><h4>Find a store</h4></a></li>
+    <li><a href="#"><h4>Order Status</h4></a></li>
+    <li><a href="#"><h4>Way to buy</h4></a></li>
+    <li><a href="#"><h4>University Student Offer</h4></a></li>
+   
+
+    <li><a href="#">Shop special store</a></li>
+    <li><a href="#"><h4>Education</h4></a></li>
+    <li><a href="#"><h4>Business</h4></a></li>
+       
+    </ul>
+
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link7;
+ }
+ function enter(){
+    let link8=`<div id="enter">
+    <ul>
+    <li><a href="#">Explore Support</a></li>
+    <li><a href="#"><h1>iphone</h1></a></li>
+    <li><a href="#"><h1>Mac</h1></a></li>
+    <li><a href="#"><h1>ipad</h1></a></li>
+    <li><a href="#"><h1>Watch</h1></a></li>
+    <li><a href="#"><h1>AirPods</h1></a></li>
+    <li><a href="#"><h1>Music</h1></a></li>
+    <li><a href="#"><h1>TV</h1></a></li>
+    <li><a href="#">Explore Support</a></li>
+
+   
+</ul>
+<ul>
+    <li><a href="#">Get Help</a></li>
+    <li><a href="#"><h4>Community</h4></a></li>
+    <li><a href="#"><h4> Check Coverage</h4></a></li>
+    <li><a href="#"><h4>Repaid</h4></a></li>
+    <li><a href="#"><h4>Contact us</a></li>
+
+   
+</ul>
+<ul>
+    <li><a href="#">Helpful Topics</a></li>
+    <li><a href="#"><h4>Get AppleCare+</h4></a></li>
+    <li><a href="#"><h4>Apple ID & Password</h4></a></li>
+    <li><a href="#"><h4>Find My</h4></a></li>
+    <li><a href="#"><h4>Accessibility</h4></a></li>
+  
+   
+</ul>
+
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link8;
+ }
+ function access(){
+    let link9=`   <div id="acces">
+    <ul>
+   >
+        <li><a href="#">Support</a></li>
+        <li><a href="#"><h1>iMac</h1></a></li>
+        <li><a href="#"><h1>Mac mini</h1></a></li>
+        <li><a href="#"><h1>Mac studio</h1></a></li>
+        <li><a href="#"><h1>Mac pro</h1></a></li>
+      
+        <li><a href="#"><h1>iMac</h1></a></li>
+        <li><a href="#"><h1>Display</h1></a></li>
+    </ul>
+    <ul>
+    <li><a href="#">Quick Links</a></li>
+    <li><a href="#"><h4>Find a store</h4></a></li>
+    <li><a href="#"><h4>Order Status</h4></a></li>
+    <li><a href="#"><h4>Way to buy</h4></a></li>
+    <li><a href="#"><h4>University Student Offer</h4></a></li>
+       
+    </ul>
+
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link9;
+ }
+ 
+ function support(){
+    let link10=`   <div id="sup">
+    <ul>
+        <li><a href="#">Explore Support</a></li>
+        <li><a href="#"><h1>iphone</h1></a></li>
+        <li><a href="#"><h1>Mac</h1></a></li>
+        <li><a href="#"><h1>ipad</h1></a></li>
+        <li><a href="#"><h1>Watch</h1></a></li>
+        <li><a href="#"><h1>AirPods</h1></a></li>
+        <li><a href="#"><h1>Music</h1></a></li>
+        <li><a href="#"><h1>TV</h1></a></li>
+        <li><a href="#">Explore Support</a></li>
+    
+       
+    </ul>
+    <ul>
+        <li><a href="#">Get Help</a></li>
+        <li><a href="#"><h4>Community</h4></a></li>
+        <li><a href="#"><h4> Check Coverage</h4></a></li>
+        <li><a href="#"><h4>Repaid</h4></a></li>
+        <li><a href="#"><h4>Contact us</a></li>
+
+       
+    </ul>
+    <ul>
+        <li><a href="#">Helpful Topics</a></li>
+        <li><a href="#"><h4>Get AppleCare+</h4></a></li>
+        <li><a href="#"><h4>Apple ID & Password</h4></a></li>
+        <li><a href="#"><h4>Find My</h4></a></li>
+        <li><a href="#"><h4>Accessibility</h4></a></li>
+      
+       
+    </ul>
+    </div> `;
+    document.getElementById('finaloutput').innerHTML= link10;
+ }
+
+
